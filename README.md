bff81c8ff77d8c434920358a7c895c5e31002714
Adam Culp <adamculp@uws.net>
2020-02-11 16:20:45 -0500

    Ready code example

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
-# voice-aws-transcribe-php
-PHP script to transcribe voice to text using AWS Transcribe service
+# SMS Sentiment Analysis with IBM Tone Analyzer using PHP
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://nexmo.dev/voice-transcribe-php-heroku)
+
+asdfadsfasd
+
+
+
+
+
+
+
+
+## AWS Services
+
+For this example we will multiple AWS services, such as [AWS S3](https://aws.amazon.com/s3/) and [Amazon Transcribe](https://aws.amazon.com/transcribe/).
+
+In S3, create a bucket that will be used to store the MP3 files retrieved from Nexmo. Then make note of the bucket name for later inclusion to the `.env` file.
+
+Using the [AWS IAM](https://aws.amazon.com/iam/), add a new user and add the following permissions by importing the following JSON.
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "transcribe:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:DeleteObject"
+        ],
+            "Resource": "arn:aws:s3:::{bucket_name}/*"
+        }
+    ]
+}
+```
+
+
+## Running the App
+
+This sample app uses a `.env` file to provide the API key and URL to the IBM Tone Analyzer API.
+
+Copy the provided `.env.example` file to a new file called `.env`:
+
+```
+cp .env.example > .env
+```
+
+Then update the values with those from the IBM Tone Analyzer service Manage page, and then save.
+
+```
+APP_ID=voice-aws-transcribe-php
+LANG_CODE=en-US
+SAMPLE_RATE=8000
+AWS_REGION=us-east-1
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_S3_BUCKET_NAME=
+AWS_S3_RECORDING_FOLDER_NAME=
+NEXMO_APPLICATION_PRIVATE_KEY_PATH=
+NEXMO_APPLICATION_ID=
+```
+
+### Use Composer to install dependencies
+
+Both methods of serving the app, shown below, do require the use of Composer to install dependencies and set up the autoloader.
+
+Assuming a Composer global installation. [https://getcomposer.org/doc/00-intro.md#globally](https://getcomposer.org/doc/00-intro.md#globally)
+
+```
+composer install
+```
+
+#### Launching the PHP built-in webserver
+
+We will utilize the PHP built-in webserver for this sample application. Though it not intended for production, it is fine for sample/test usage. To serve the app run the following command in a terminal from the application directory:
+
+```
+php -S localhost:8080
+```
+
+## Launch ngrok
+To enable the Nexmo callback to reach the application, expose the application to the internet using [ngrok](https://ngrok.com/). To see how, [check out this guide](https://www.nexmo.com/blog/2017/07/04/local-development-nexmo-ngrok-tunnel-dr/).
+
+## Linking the app to Nexmo
+
+After buying a virtual phone number through the [Nexmo Dashboard](https://dashboard.nexmo.com/buy-numbers), create an application with Voice capabilities and link it to the number. Make sure to generate the public/private keys, and save the private key in the application root when prompted.
+
+As part of the Application creation, we also need to add the URLs for Event and Answer callbacks, while Fallback is not needed for this example.
+
+These URLs will need the ngrok addresses provided after launching ngrok via CLI.
+
+For example:
+```
+    Event URL = https://567d01f9.ngrok.io/webhooks/recording
+    Answer URL = https://567d01f9.ngrok.io/webhooks/answer
+```
+
+## Try it out
+
+With the example PHP application running using the PHP built-in webserver, and made available online for the callbacks with ngrok, make a phone call to the Nexmo virtual number and follow the prompts.
+
+After leaving a message, there should be an MP3 file added to the [bucket in S3](https://s3.console.aws.amazon.com/s3/) as well as a [transcribe job running in Amazon Transcribe](https://console.aws.amazon.com/transcribe/).
+
+Upon successful transcription the final text can be downloaded, or used in applications that can can connect and download it.
+
+## Blog Post
+
+For a more full explanation and example, check out the [blog post](https://www.nexmo.com/blog).
--- index.php
+++ index.php
@@ -1,3 +1,147 @@
 <?php
 
-// beginning file
+require('vendor/autoload.php');
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Nexmo\Client;
+use Nexmo\Client\Credentials\Keypair;
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+use Aws\TranscribeService\TranscribeServiceClient;
+
+Dotenv\Dotenv::create(__DIR__)->load();
+
+$app = AppFactory::create();
+
+/**
+ * Answer the call and prompt using NCCO flow
+ */
+$app->any('/webhooks/answer', function (Request $request, Response $response) {
+    $uri = $request->getUri();
+
+    if ($request->getMethod() != 'GET') {
+        return $response->withStatus(403);
+    }
+
+    // NOTE: The usage of $uri below will not work when using ngrok, as getHost() will return localhost
+    $ncco = [
+        [
+            'action' => 'talk',
+            'text' => 'Please leave a message after the tone, then press #.'
+        ],
+        [
+            'action' => 'record',
+            'eventUrl' => [
+                $uri->getScheme().'://'.$uri->getHost().':'.$uri->getPort().'/webhooks/fetch'
+            ],
+            'endOnSilence' => '3',
+            'endOnKey' => '#',
+            'beepOnStart' => true
+        ],
+        [
+            'action' => 'talk',
+            'text' => 'Thank you for your message. Goodbye.'
+        ],
+        [
+            'action' => 'notify',
+            'payload' => ['foo'=>'bar'],
+            'eventUrl' => [
+                $uri->getScheme().'://'.$uri->getHost().':'.$uri->getPort().'/webhooks/transcribe'
+            ],
+            'eventMethod' => "POST"
+        ],
+
+    ];
+
+    $response->getBody()->write(json_encode($ncco));
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+});
+
+/**
+ * Event URL used by Nexmo for update POSTs
+ */
+$app->post('/webhooks/recording', function (Request $request, Response $response) {
+
+    $params = $request->getParsedBody();
+
+    error_log($params['recording_url']);
+
+    return $response
+        ->withStatus(204);
+});
+
+/**
+ * Fetch the voice recording from Nexmo, store on AWS S3
+ */
+$app->post('/webhooks/fetch', function (Request $request, Response $response) {
+
+    $params = json_decode($request->getBody(), true);
+
+    // Create Nexmo Client
+    $keypair = new Keypair(
+        file_get_contents($_ENV['NEXMO_APPLICATION_PRIVATE_KEY_PATH']),
+        $_ENV['NEXMO_APPLICATION_ID']
+    );
+
+    $nexmoClient = new Client($keypair);
+
+    $data = $nexmoClient->get($params['recording_url']);
+
+    // Create AWS S3 Client
+    $S3Client = new S3Client([
+        'credentials' => [
+            'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+            'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+        ],
+        'region' => $_ENV['AWS_REGION'],
+        'version' => 'latest',
+    ]);
+
+    $adapter = new AwsS3Adapter($S3Client, $_ENV['AWS_S3_BUCKET_NAME']);
+
+    $filesystem = new Filesystem($adapter);
+
+    $filesystem->put('/' . $_ENV['AWS_S3_RECORDING_FOLDER_NAME'] .'/'.$params['conversation_uuid'].'.mp3', $data->getBody());
+
+    return $response
+        ->withStatus(204);
+});
+
+/**
+ * Request Amazon Transcription of the voice file
+ */
+$app->post('/webhooks/transcribe', function (Request $request, Response $response) {
+
+    $params = json_decode($request->getBody(), true);
+
+    // Create Amazon Transcribe Client
+    $awsTranscribeClient = new TranscribeServiceClient([
+        'credentials' => [
+            'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+            'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+        ],
+        'region' => $_ENV['AWS_REGION'],
+        'version' => 'latest',
+    ]);
+
+    $transcriptionResult = $awsTranscribeClient->startTranscriptionJob([
+            'LanguageCode' => 'en-US',
+            'Media' => [
+                'MediaFileUri' => 'https://' . $_ENV['AWS_S3_BUCKET_NAME'] . '.s3.amazonaws.com/' . $_ENV['AWS_S3_RECORDING_FOLDER_NAME'] . '/' . $params['conversation_uuid'] . '.mp3',
+            ],
+            'MediaFormat' => 'mp3',
+            'TranscriptionJobName' => 'nexmo_voice_' . $params['conversation_uuid'],
+    ]);
+
+    $response->getBody()->write(json_encode($transcriptionResult->toArray()));
+
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus(204);
+});
+
+$app->run();
